93478749828929d9b88cdb2848681592630e0ed0
Yanjie Li <aj7105@gmail.com>
2023-02-26 14:32:18 +0800

    Initial commit

--- R/app_old2.R
+++ R/app_old2.R
@@ -19,10 +19,10 @@ SpecexR_app <- function() {
   if (length(setdiff(pkgs, rownames(installed.packages()))) > 0)
     BiocManager::install(pkgs)
 
-  packages <- c("shinythemes", 'shinyWidgets', 'terra', 'htmltools','shinyjs', 'RCSF','DT',
+  packages <- c("shinythemes", 'shinyWidgets', 'terra', 'shinyjs', 'RCSF','DT',
                 "shinydashboard",'stars','colorspace','readr',
-                'sfheaders','sf','exactextractr', 'lidR', "shiny",'tidyverse',
-                'RStoolbox','viridis', 'rgdal','tictoc',  'ggrepel',
+                'sfheaders','sf','exactextractr', 'lidR' ,'tidyverse',
+                'viridis', 'rgdal','tictoc',  'ggrepel',
                 'raster', 'tools','rasterVis','data.table'  )
 
   new.packages <- packages[!(packages %in% utils::installed.packages()[,"Package"])]
